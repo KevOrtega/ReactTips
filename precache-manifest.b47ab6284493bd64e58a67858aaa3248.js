03889a4eebe5814d9a254b297106ab9e2bd5d506
Kevin <kevinortega.dev@gmail.com>
2020-10-15 12:19:17 -0300

    first commit

---
+++ precache-manifest.b47ab6284493bd64e58a67858aaa3248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e284f3f34fbde62039e55a8358401b2",
+    "url": "/KevOrtega/ReactTips/index.html"
+  },
+  {
+    "revision": "9e47ffd00225428d95f8",
+    "url": "/KevOrtega/ReactTips/static/css/main.9024b76d.chunk.css"
+  },
+  {
+    "revision": "5ecf0f59b75d8f9d28be",
+    "url": "/KevOrtega/ReactTips/static/js/2.0696d96f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KevOrtega/ReactTips/static/js/2.0696d96f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e47ffd00225428d95f8",
+    "url": "/KevOrtega/ReactTips/static/js/main.e4096272.chunk.js"
+  },
+  {
+    "revision": "cf9ce4c89b8e3e2142da",
+    "url": "/KevOrtega/ReactTips/static/js/runtime-main.9f885d2e.js"
+  }
+]);
